cf7555c39ec2b8c8ddfc372e3256f7be6d500495
r-4avi5 <ravi_k@es.iitr.ac.in>
2026-07-15 18:13:37 +0530

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Agence from './pages/Agence'
+import Navbar from './components/navigation/Navbar'
+import FullScreenNav from './components/navigation/FullScreenNav'
 
 function App() {
 return (
-    <div>
+    <div className='overflow-x-hidden'>
+      <Navbar/>
+      <FullScreenNav/>
     <Routes>
       <Route path='/' element ={<Home/>}/>
       <Route path='/agence' element ={<Agence/>}/>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Stairs from './components/common/Stairs.jsx'
+import NavContext from './context/NavContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -11,7 +12,9 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     
     <Stairs>
-      <App />
+      <NavContext>
+        <App />
+      </NavContext>
     </Stairs>
     
     </BrowserRouter>

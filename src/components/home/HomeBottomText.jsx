ad46e2ef4949e54fea0b3f52bb1c528c1b823dd1
r-4avi5 <ravi_k@es.iitr.ac.in>
2026-07-18 12:34:25 +0530

    responsive also

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -5,10 +5,10 @@ function HomeBottomText() {
   return (
     <div className='font-[font2] flex items-center justify-center gap-5'>
       <div className='lg:h-25 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-3 border-white rounded-full px-2 pt-2 lg:px-8 uppercase'>
-        <Link className='text-[5vw] lg:mt-3'to={'/projects'} >Projects</Link>
+        <Link className='text-[5vw]'to={'/projects'} >Projects</Link>
       </div>
       <div className='lg:h-25 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-3 border-white rounded-full px-2 pt-2 lg:px-8 uppercase'>
-        <Link className='text-[5vw] lg:mt-3'to={'/agence'} >agence</Link>
+        <Link className='text-[5vw]'to={'/agence'} >agence</Link>
       </div>
     </div>
   )

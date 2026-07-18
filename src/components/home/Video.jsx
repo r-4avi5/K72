90af8e9a10c82c93a07ef025e839f9a56a13d44b
r-4avi5 <ravi_k@es.iitr.ac.in>
2026-07-18 12:29:41 +0530

    Fix video path to absolute

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../public/fonts/video.mp4"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="/fonts/video.mp4"></video>
     </div>
   )
 }
